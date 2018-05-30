f4cb4fb99094d8945874da63f2cad94dfa8b9029
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-30 14:49:25 +0200

    Change all js files

--- app/program.js
+++ app/program.js
@@ -1,44 +1,29 @@
-var OSinfo = require('./modules/OSInfo');
-var os = require('os');
-
+var OSinfo = require('../modules/OSInfo.js');
 
 process.stdin.setEncoding('utf-8');
-
-process.stdin.on('readable', function(timeCalculator) {
-
-    var minutes = Math.floor(seconds / 60),
-        hours = Math.floor(seconds / 3600);
-
-    var seconds= process.stdin.read();
-	    if (seconds !== null) {
-	        var instruction = seconds.toString().trim();
-
-
-	function timeCalculator(seconds) {
-
-	    switch (seconds) {
-
-	    	case seconds = NaN:
-	    		process.stdout.write("Wpisano złą wartość\n");
-			    process.exit();
+process.stdin.on('readable', function(){
+	var input = process.stdin.read();
+	if (input !== null) {
+		var instruction = input.toString().trim();
+		switch(instruction) {
+			case '/exit':
+				process.stdout.write('Quitting app!\n');
+				process.exit();
+				break;
+			case '/lang':
+				process.stdout.write(process.env.LANG + '\n');
+				break;
+			case '/ver':
+				process.stdout.write(process.versions.node + '\n');
+				break;
+			case '/sayhello':
+			    process.stdout.write('hello! :)\n');
 			    break;
-
-			case seconds < 0:
-	    		process.stdout.write("Wpisano ujemną wartość\n");
-			    process.exit();
+			case '/getOSinfo':
+			    OSinfo.print();
 			    break;
-
-			case seconds > 0 & seconds !< 3600:
-	    		process.stdout.write(minutes);
-			    process.exit();
-			    break;
-
-			default seconds > 3600:
-	    		process.stdout.write(hours);
-			    process.exit();
-			}
+			default:
+				process.stderr.write('Wrong instruction!\n');
 		}
 	}
-});
-
-exports.print = timeCalculator;
+})
