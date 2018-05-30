cd1e3157baba23b18833827543b357c23238869e
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-30 11:28:24 +0200

    Change program file

--- app/program.js
+++ app/program.js
@@ -11,34 +11,34 @@ function timeCalculator(seconds) {
 	var seconds= process.stdin.read();
     if (seconds !== null) {
         var instruction = seconds.toString().trim();
-    }
+    
 
     var minutes = Math.floor(seconds / 60),
         hours = Math.floor(seconds / 3600);
 
-     switch (instruction) {
+	    switch (instruction) {
 
-    	case seconds = NaN:
-    		process.stdout.write("Wpisano złą wartość\n");
-		    process.exit();
-		    break;
+	    	case seconds = NaN:
+	    		process.stdout.write("Wpisano złą wartość\n");
+			    process.exit();
+			    break;
 
-		case seconds < 0:
-    		process.stdout.write("Wpisano ujemną wartość\n");
-		    process.exit();
-		    break;
+			case seconds < 0:
+	    		process.stdout.write("Wpisano ujemną wartość\n");
+			    process.exit();
+			    break;
 
-		case seconds > 0 & seconds !< 3600:
-    		process.stdout.write(minutes);
-		    process.exit();
-		    break;
+			case seconds > 0 & seconds !< 3600:
+	    		process.stdout.write(minutes);
+			    process.exit();
+			    break;
 
-		default seconds > 3600:
-    		process.stdout.write(hours);
-		    process.exit();
-    }
+			default seconds > 3600:
+	    		process.stdout.write(hours);
+			    process.exit();
+	    }
 
-
+	}
 }
 
 exports.print = timeCalculator;
