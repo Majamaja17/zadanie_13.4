28ac1ae1711ad27a031e2c78239c6b4fc94e92b2
Maja Jelenik <maja.jelenik@vp.pl>
2018-05-30 14:58:18 +0200

    Change js files

--- app/program.js
+++ app/program.js
@@ -3,13 +3,12 @@ var OSinfo = require('../modules/OSInfo.js');
 process.stdin.setEncoding('utf-8');
 process.stdin.on('readable', function(){
 
-		var os = require('os');
+		var time = require('os');
 
 		function getTime() {
-			let uptime = Math.floor(os.uptime());
+			let uptime = Math.floor(time.uptime());
 			let hours = Math.floor(uptime / 3600);
-			let minutes = Math.floor((uptime - (hours * 3600)) / 60);
-			uptime = uptime % 60;
+			let minutes = Math.floor(uptime / 60);
 			return hours + ' hours ' + minutes + ' minutes ' + uptime + ' seconds'
 		}
 
