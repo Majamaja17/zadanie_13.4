299cef1e7bd7fb6f536d28d55a6d7e6767604da2
Maja Jelenik <maja.jelenik@vp.pl>
2018-06-01 13:24:40 +0200

    Change program.js

--- app/program.js
+++ app/program.js
@@ -1,6 +1,6 @@
-var OSinfo = require('/OSInfo.js');
-
+var OSinfo = require('../modules/OSInfo.js');
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('readable', function(){
 
 	var time = require('os');
@@ -8,8 +8,9 @@ process.stdin.on('readable', function(){
 	function getTime() {
 		let uptime = Math.floor(time.uptime());
 		let hours = Math.floor(uptime / 3600);
-		let minutes = Math.floor(uptime / 60);
-		return hours + ' hours ' + minutes + ' minutes ' + uptime + ' seconds'
+		let minutes = Math.floor((uptime - (hours*3600) / 60);
+		let seconds = Math.floor(uptime -hours * 3600 - minutes*60);
+		return hours + ' hours ' + minutes + ' minutes ' + seconds + ' seconds'
 	}
 
 	exports.time = getTime;
